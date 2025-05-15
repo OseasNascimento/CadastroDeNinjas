96d6ae4cff6c4d58d1642fcbb69ce96237cd39a9
Oséas nascimento <oseasoliveira.nascimento@gmail.com>
2025-05-15 12:34:33 -0300

    Criado a controller

---
+++ src/main/java/nascimento/aula/CadastrodeNinjas/Controller.java
@@ -0,0 +1,17 @@
+package nascimento.aula.CadastrodeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas () {
+      return "Essa é minha primeira menssagem nesta rota";
+    }
+
+
+}
